45c176aa78176c2a28a8a3a257e959bf9111a667
Luca Foscili <45429703+lucafoscili@users.noreply.github.com>
2024-09-22 11:00:29 +0200

    fix (chat): system textarea style

--- packages/ketchup-lite/src/components/kul-chat/settings/settings.tsx
+++ packages/ketchup-lite/src/components/kul-chat/settings/settings.tsx
@@ -14,83 +14,19 @@ export const OPTIONS_IDS = {
 export const prepSettings = (adapter: KulChatAdapter) => {
     return (
         <Fragment>
-            <div class="settings">
-                {prepButton(adapter)}
-                <div class="settings__options">
-                    <kul-textfield
-                        id={OPTIONS_IDS.temperature}
-                        kulHtmlAttributes={{
-                            min: 0,
-                            step: 0.1,
-                            type: 'number',
-                        }}
-                        kulIcon="thermometer"
-                        kulLabel="Temperature"
-                        kulValue={String(
-                            adapter.get.props.temperature()
-                        ).valueOf()}
-                        onKul-textfield-event={textfieldEventHandler.bind(
-                            textfieldEventHandler,
-                            adapter
-                        )}
-                    ></kul-textfield>
-                    <kul-textfield
-                        id={OPTIONS_IDS.endpointUrl}
-                        kulIcon="http"
-                        kulLabel="Endpoint URL"
-                        kulValue={adapter.get.props.endpointUrl()}
-                        onKul-textfield-event={textfieldEventHandler.bind(
-                            textfieldEventHandler,
-                            adapter
-                        )}
-                    ></kul-textfield>
-                    <kul-textfield
-                        id={OPTIONS_IDS.maxTokens}
-                        kulHtmlAttributes={{
-                            min: 10,
-                            step: 100,
-                            type: 'number',
-                        }}
-                        kulIcon="plus_one"
-                        kulLabel="Max tokens count"
-                        kulValue={String(
-                            adapter.get.props.maxTokens()
-                        ).valueOf()}
-                        onKul-textfield-event={textfieldEventHandler.bind(
-                            textfieldEventHandler,
-                            adapter
-                        )}
-                    ></kul-textfield>
-                    <kul-textfield
-                        id={OPTIONS_IDS.polling}
-                        kulHtmlAttributes={{
-                            min: 10,
-                            step: 10,
-                            type: 'number',
-                        }}
-                        kulIcon="timer"
-                        kulLabel="Polling interval"
-                        kulValue={String(
-                            adapter.get.props.pollingInterval()
-                        ).valueOf()}
-                        onKul-textfield-event={textfieldEventHandler.bind(
-                            textfieldEventHandler,
-                            adapter
-                        )}
-                    ></kul-textfield>
-                    <kul-textfield
-                        id={OPTIONS_IDS.system}
-                        class="settings__options__system"
-                        kulLabel="System prompt"
-                        kulStyling="textarea"
-                        kulValue={adapter.get.props.system()}
-                        onKul-textfield-event={textfieldEventHandler.bind(
-                            textfieldEventHandler,
-                            adapter
-                        )}
-                    ></kul-textfield>
-                </div>
-            </div>
+            {prepButton(adapter)}
+            <div class="settings__options">{prepFields(adapter)}</div>
+            <kul-textfield
+                id={OPTIONS_IDS.system}
+                class="settings__system kul-full-height"
+                kulLabel="System prompt"
+                kulStyling="textarea"
+                kulValue={adapter.get.props.system()}
+                onKul-textfield-event={textfieldEventHandler.bind(
+                    textfieldEventHandler,
+                    adapter
+                )}
+            ></kul-textfield>
         </Fragment>
     );
 };
@@ -106,6 +42,68 @@ const prepButton = (adapter: KulChatAdapter) => {
                 adapter
             )}
         ></kul-button>
+    );
+};
+
+const prepFields = (adapter: KulChatAdapter) => {
+    return (
+        <Fragment>
+            <kul-textfield
+                id={OPTIONS_IDS.temperature}
+                kulHtmlAttributes={{
+                    min: 0,
+                    step: 0.1,
+                    type: 'number',
+                }}
+                kulIcon="thermometer"
+                kulLabel="Temperature"
+                kulValue={String(adapter.get.props.temperature()).valueOf()}
+                onKul-textfield-event={textfieldEventHandler.bind(
+                    textfieldEventHandler,
+                    adapter
+                )}
+            ></kul-textfield>
+            <kul-textfield
+                id={OPTIONS_IDS.endpointUrl}
+                kulIcon="http"
+                kulLabel="Endpoint URL"
+                kulValue={adapter.get.props.endpointUrl()}
+                onKul-textfield-event={textfieldEventHandler.bind(
+                    textfieldEventHandler,
+                    adapter
+                )}
+            ></kul-textfield>
+            <kul-textfield
+                id={OPTIONS_IDS.maxTokens}
+                kulHtmlAttributes={{
+                    min: 10,
+                    step: 100,
+                    type: 'number',
+                }}
+                kulIcon="plus_one"
+                kulLabel="Max tokens count"
+                kulValue={String(adapter.get.props.maxTokens()).valueOf()}
+                onKul-textfield-event={textfieldEventHandler.bind(
+                    textfieldEventHandler,
+                    adapter
+                )}
+            ></kul-textfield>
+            <kul-textfield
+                id={OPTIONS_IDS.polling}
+                kulHtmlAttributes={{
+                    min: 10,
+                    step: 10,
+                    type: 'number',
+                }}
+                kulIcon="timer"
+                kulLabel="Polling interval"
+                kulValue={String(adapter.get.props.pollingInterval()).valueOf()}
+                onKul-textfield-event={textfieldEventHandler.bind(
+                    textfieldEventHandler,
+                    adapter
+                )}
+            ></kul-textfield>
+        </Fragment>
     );
 };
 
