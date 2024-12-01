9678782654930e92e3dcddc02d9f60db0755570d
Luca Foscili <45429703+lucafoscili@users.noreply.github.com>
2024-12-01 22:59:46 +0100

    fix(ssr): changed path to setAssetPath

--- packages/ketchup-lite-react-ssr/lib/index.ts
+++ packages/ketchup-lite-react-ssr/lib/index.ts
@@ -1,7 +1,3 @@
-// ketchup-lite-react-ssr/lib/index.js
+export { setAssetPath } from '../node_modules/ketchup-lite/dist/components/index';
 
-// Export utility functions like setAssetPath first
-export { setAssetPath } from 'ketchup-lite/dist/components/index';
-
-// Then export the Stencil components
 export * from './components/stencil-generated/components';
