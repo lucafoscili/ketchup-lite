316f795dca48ff9cbb285c22e3dd9d7c2bcff0e5
Luca Foscili <45429703+lucafoscili@users.noreply.github.com>
2024-12-19 22:05:54 +0100

    fix(kulmanager): initialization correctly awaits for the ready event

--- packages/ketchup-lite/src/index.ts
+++ packages/ketchup-lite/src/index.ts
@@ -5,34 +5,41 @@ import { KulManager } from "./managers/kul-manager/kul-manager";
 const KulManagerSymbol = Symbol("KulManager");
 
 const kulManagerSingleton: KulManager = (() => {
-  const globalScope = globalThis;
+  const globalScope = globalThis as any;
 
   if (!globalScope[KulManagerSymbol]) {
     const kulManager = new KulManager(getAssetPath, setAssetPath);
-    const { dates, language, theme } = kulManager;
-    const { locale } = dates;
-
-    const defaultLanguage =
-      KulLanguageDefaults[locale] ?? KulLanguageDefaults.en;
-    language.set(defaultLanguage);
-
-    theme.set();
-
     globalScope[KulManagerSymbol] = kulManager;
 
     document.dispatchEvent(new CustomEvent("kul-manager-ready"));
+
     if (process.env.NODE_ENV === "development") {
       globalScope.kulManager = kulManager;
-      console.debug("KulManager initialized and ready.");
+      console.debug(
+        "KulManager instance created. Waiting for initialization...",
+      );
     }
   }
 
   return globalScope[KulManagerSymbol];
 })();
 
-/**
- * Singleton instance of the KulManager. Automatically initialized and ready to use.
- */
-export { kulManagerSingleton };
+function onKulManagerReady() {
+  const { dates, language, theme } = kulManagerSingleton;
+  const { locale } = dates;
+  const defaultLanguage = KulLanguageDefaults[locale] ?? KulLanguageDefaults.en;
 
+  language.set(defaultLanguage);
+  theme.set();
+
+  if (process.env.NODE_ENV === "development") {
+    console.debug("KulManager fully initialized with language and theme.");
+  }
+
+  document.removeEventListener("kul-manager-ready", onKulManagerReady);
+}
+
+document.addEventListener("kul-manager-ready", onKulManagerReady);
+
+export { kulManagerSingleton };
 export * from "./components";
